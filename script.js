391871316445dd330a78de6bf92fbd0defa4537d
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2022-06-02 18:50:05 -0700

    test1

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-//My javascript stuff
-console.log("Yay it kinda works");
+const contain = document.querySelector('section');
+const press = document.querySelector('button');
+let params = {
+	fitted: true
+};
+const two = new Two(params);
+two.appendTo(contain);
+run();
+
+function run() {
+	let fate = Math.floor(Math.random() * 50) + 5;
+	let x, y, w, h, col = 0;
+	let shape = null;
+	const cols = ['#4d0000', '#ff1a1a', '#ff8080', '#791616', '#d72828', '#ff6600', '#993d00', '#ff8533', '#bd4b00', '#ff9900'];
+	for(let i = 0; i < fate; i++){
+		x = Math.floor(Math.random() * contain.offsetWidth);
+		y = Math.floor(Math.random() * contain.offsetHeight);
+		w = Math.floor(Math.random() * (contain.offsetWidth - 100) + 10);
+		h = Math.floor(Math.random() * (contain.offsetHeight - 100) + 10);
+		col = Math.floor(Math.random() * cols.length);
+	
+		shape = two.makeRectangle(x,y,w,h);
+		if(fate % 2 == 0)
+			shape.noStroke();
+		shape.fill = cols[col];
+	}
+	two.play();
+}
+
+press.addEventListener('click', () => {
+    run();
+});
